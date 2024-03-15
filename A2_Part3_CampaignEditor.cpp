bfb24998470f8aba0299b1f9d6adc16edabf7955
Ulas Duman <65922418+uduman@users.noreply.github.com>
2024-03-15 19:55:33 -0400

    Changes

--- A2_Part3_CampaignEditor.cpp
+++ A2_Part3_CampaignEditor.cpp
@@ -4,238 +4,105 @@
 #include <string>
 #include "Part2_Map.cpp"
 
+using std::cin;
 using std::cout;
 using std::endl;
 using std::ifstream;
 using std::ofstream;
 using std::string;
 using std::vector;
-using std::cin;
 
-class Campaign
+class CampaignEditor
 {
+private:
+    Map campaign;
 
 public:
-    vector<Map> maps;
-    int canvasWidth = 200;
-    int canvasHeight = 20;
-    vector<vector<int>> canvas;
-    int cursorX = 0;
-    int cursorY = 0;
+    CampaignEditor() : campaign(0,0) {}
 
-    Campaign()
+    void appendMap(Map map)
     {
-        canvas = vector<vector<int>>(canvasWidth, vector<int>(canvasHeight, 0));
+        campaign.map.insert(campaign.map.end(), map.map.begin(), map.map.end());
     }
 
-    void addMap(Map map)
+    void start()
     {
-        maps.push_back(map);
     }
 
-    void moveCursor(int x, int y)
+    int update()
     {
-        // Check if the cursor will be out of bounds after moving
-        if (cursorX + x < 0 || cursorX + x >= canvasWidth || cursorY + y < 0 || cursorY + y >= canvasHeight)
+        // Interactive loop
+        char input;
+        string filename;
+
+        while (true)
         {
-            cout << "Cursor will be out of bounds." << endl;
-            return;
-        }
-        cursorX += x;
-        cursorY += y;
-    }
+            cout << "Enter a hotkey [q]uit, [c]reate a new map + [width],[height],[s]ave, l[o]ad, [m]odify + (1,2,3) to change type, hjkl to move cursor: ";
 
-    void printCanvas()
-    {
-        for (int i = 0; i < canvasHeight; i++)
-        {
-            for (int j = 0; j < canvasWidth; j++)
+            switch (input)
             {
-                // Cursor position represented by 'O'
-                if (cursorX == j && cursorY == i)
-                {
-                    cout << "o  ";
-                }
-                else if (canvas[j][i] == 1)
-                {
-                    cout << ".  ";
-                }
-                else if (canvas[j][i] == 2)
-                {
-                    cout << "x  ";
-                }
-                else if (canvas[j][i] == 3)
-                {
-                    cout << "|  ";
-                }
-                else
-                {
-                    // Empty canvas cell
-                    cout << "   ";
-                }
+            case 'q':
+                // Quit the program
+                return 0;
+            case 's':
+                // Save the campaign
+                cout << "Enter a filename: ";
+                cin >> filename;
+                saveCampaign(filename);
+                cout << "CampaignEditor saved." << endl;
+                break;
+            case 'o':
+                // Load a campaign
+                cout << "Enter a filename: ";
+                cin >> filename;
+                loadCampaign(filename);
+                cout << "CampaignEditor loaded." << endl;
+                break;
+            case 'm':
+                // Modify the cell at the cursor
+                cout << "Enter a new type for the cell (1, 2, 3): " << endl;
+                int newType;
+                cin >> newType;
+                campaign.(campaign.cursorX, campaign.cursorY, newType);
+                break;
+            case 'h':
+                // Move the cursor left
+                campaign.moveCharacter(-1, 0);
+                break;
+            case 'j':
+                // Move the cursor down
+                campaign.moveCharacter(0, 1);
+                break;
+            case 'k':
+                // Move the cursor up
+                campaign.moveCharacter(0, -1);
+                break;
+            case 'l':
+                // Move the cursor right
+                campaign.moveCharacter(1, 0);
+                break;
+            default:
+                cout << "Invalid hotkey. Try again." << endl;
+                break;
             }
-            cout << endl;
+
+            draw();
         }
+
+        return 0;
+    }
+
+    void draw()
+    {
+        printf("\033c"); // clears terminal
+        campaign.printMap();
+    }
+
+    void loadCampaign(string filename)
+    {
     }
 
     void saveCampaign(string filename)
     {
-        ofstream file(filename);
-        if (file.is_open())
-        {
-            for (int i = 0; i < canvasHeight; i++)
-            {
-                for (int j = 0; j < canvasWidth; j++)
-                {
-                    file << canvas[j][i] << " ";
-                }
-                file << endl;
-            }
-        }
-        else
-        {
-            cout << "Unable to open file." << endl;
-        }
-    }
-
-    // Load a campaign from a file
-    void loadCampaign(string filename)
-    {
-        ifstream file(filename);
-        if (file.is_open())
-        {
-            for (int i = 0; i < canvasHeight; i++)
-            {
-                for (int j = 0; j < canvasWidth; j++)
-                {
-                    file >> canvas[j][i];
-                }
-            }
-        }
-        else
-        {
-            cout << "Unable to open file." << endl;
-        }
-        file.close();
-    }
-
-    bool isValid()
-    {
-        return true;
-    }
-
-    void changeCellType(int x, int y, int newType)
-    {
-        canvas[x][y] = newType;
     }
 };
-
-int main()
-{
-    Map map1(10, 10);
-    Map map2(15, 15);
-    Map map3(9, 15);
-
-    Campaign campaign;
-    campaign.addMap(map1);
-    campaign.addMap(map2);
-    campaign.addMap(map3);
-
-
-    // Accumulate starting positions for each map to render them next to each other
-    int startingPosAcc = 0;
-
-    // Add the maps to the canvas
-    for (int i = 0; i < campaign.maps.size(); i++)
-    {
-        Map map = campaign.maps[i];
-        int mapSizeX = map.getMap().size();
-        int mapSizeY = map.getMap()[0].size();
-
-        int startX = startingPosAcc;
-
-        // Copy the map onto the canvas
-        for (int x = 0; x < mapSizeX; x++)
-        {
-            for (int y = 0; y < mapSizeY; y++)
-            {
-                campaign.canvas[startX + x][y] = map.getMap()[x][y];
-            }
-        }
-        startingPosAcc += mapSizeX;
-    }
-
-    // Print the canvas initially
-    campaign.printCanvas();
-
-    // Interactive loop
-    char input;
-    string filename;
-    int width, height;
-    while (true)
-    {
-        cout << "Enter a hotkey [q]uit, [c]reate a new map + [width],[height],[s]ave, l[o]ad, [m]odify + (1,2,3) to change type, hjkl to move cursor: ";
-        cin >> input;
-        cout << "X: " << campaign.cursorX << " Y: " << campaign.cursorY << endl;
-
-        switch (input)
-        {
-        case 'q':
-            // Quit the program
-            return 0;
-        case 'c':
-            cout << "Enter width: ";
-            cin >> width;
-            cout << " Enter height: ";
-            cin >> height;
-            cout << width << " " << height << endl;
-            campaign.addMap(Map(width,height));
-            break;
-        case 's':
-            // Save the campaign
-            cout << "Enter a filename: ";
-            cin >> filename;
-            campaign.saveCampaign(filename);
-            cout << "Campaign saved." << endl;
-            break;
-        case 'o':
-            // Load a campaign
-            cout << "Enter a filename: ";
-            cin >> filename;
-            campaign.loadCampaign(filename);
-            cout << "Campaign loaded." << endl;
-            break;
-        case 'm':
-            // Modify the cell at the cursor
-            cout << "Enter a new type for the cell (1, 2, 3): " << endl;
-            ;
-            int newType;
-            cin >> newType;
-            campaign.changeCellType(campaign.cursorX, campaign.cursorY, newType);
-            break;
-        case 'h':
-            // Move the cursor left
-            campaign.moveCursor(-1, 0);
-            break;
-        case 'j':
-            // Move the cursor down
-            campaign.moveCursor(0, 1);
-            break;
-        case 'k':
-            // Move the cursor up
-            campaign.moveCursor(0, -1);
-            break;
-        case 'l':
-            // Move the cursor right
-            campaign.moveCursor(1, 0);
-            break;
-        default:
-            cout << "Invalid hotkey. Try again." << endl;
-            break;
-        }
-        // Reprint canvas to reflect changes
-        campaign.printCanvas();
-    }
-
-    return 0;
-}
