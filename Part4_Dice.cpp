3deface1bf08f337558d5a9af1d650ba36b0df43
Thien An <tranthienan@Thien-Ans-Air.lan>
2024-03-22 17:39:30 -0400

    added dice observer

--- Part4_Dice.cpp
+++ Part4_Dice.cpp
@@ -1,126 +1,162 @@
 #include <iostream>
 #include <cstdlib>
 #include <string>
+#include <vector>
 #include "Part4_Dice.h"
+#include "Observable.cpp"
 using namespace std;
 
-//constructor
-Dice::Dice() {
+class Dice : Observable {
 
-   //generate srand unsigned int
-    srand(static_cast<unsigned int>(time(nullptr)));
-}
 
-//generates number based on sides between [1, sides]
-int Dice::rollOnce(int sides) {
+private:
+    //generates number based on sides between [1, sides]
+    int rollOnce(int sides) {
+                return (rand() % sides) + 1;
+    }
+    vector<int> results;
+    vector<Observer*> observers;
 
-    return (rand() % sides) + 1;
-}
-
-//generate result based on expression xdy[+z]
-int Dice::rollDice(string& expression) {
-
-    int num, sides, modifier = 0;
-    char d;
-
-    //find d
-    size_t posD = expression.find('d');
-    if (posD == string::npos) { //if 'd' not found in the expression, gives an error
-
-        cerr << "Invalid dice expression\n";
-        return 0;
+public:
+      
+    void addObserver(Observer* observer) {
+        observers.push_back(observer);
     }
 
-    //find + for modifier
-    int posModifier = expression.find('+');
-
-    //create substring for num and modifiier
-    try {
-
-        num = stoi(expression.substr(0, posD)); //parse sring into int
-        sides = stoi(expression.substr(posD + 1, posModifier));
-
-    } catch (const exception& e) { //throws an exception if the expression is not formatted correctly
-        cerr << "Invalid dice expression\n";
-        return 0;
-
+    void removeObserver(Observer* observer) {
+        auto it = std::find(observers.begin(), observers.end(), observer);
+        if (it != observers.end()) {
+            observers.erase(it);
+        }
+    }
+    void notifyObservers() {
+        for (Observer* observer : observers) {
+            observer->update();
+        }
     }
 
-    //validate dice -> num is at least 1, d4 d6 d8 d10 d12 d20 d100
-    if (!(num > 0) || !(sides == 4 || sides == 6 || sides == 8 || sides == 10 || sides == 12 || sides == 20 || sides == 100)) {
-
-        cerr << "Invalid dice expression\n";
-        return 0;
-
+    //constructor
+    Dice() {
+        srand(static_cast<unsigned int>(time(nullptr)));
     }
 
-    //add modifier
-    if (posModifier != string::npos) {
+    //generate result based on expression xdy[+z]
+    int rollDice(string& expression) {
 
+        int num, sides, modifier = 0;
+        char d;
+
+        //find d
+        size_t posD = expression.find('d');
+        if (posD == string::npos) { //if 'd' not found in the expression, gives an error
+
+            cerr << "Invalid dice expression\n";
+            return 0;
+        }
+
+        //find + for modifier
+        int posModifier = expression.find('+');
+
+        //create substring for num and modifiier
         try {
-            modifier = stoi(expression.substr(posModifier + 1, expression.length() - 1)); //create substring for modifier and parse into integer
 
-        } catch (const exception& e) {
-            cerr << "Invalid modifier\n";
+            num = stoi(expression.substr(0, posD)); //parse sring into int
+            sides = stoi(expression.substr(posD + 1, posModifier));
+
+        } catch (const exception& e) { //throws an exception if the expression is not formatted correctly
+            cerr << "Invalid dice expression\n";
             return 0;
 
         }
 
+        //validate dice -> num is at least 1, d4 d6 d8 d10 d12 d20 d100
+        if (!(num > 0) || !(sides == 4 || sides == 6 || sides == 8 || sides == 10 || sides == 12 || sides == 20 || sides == 100)) {
+
+            cerr << "Invalid dice expression\n";
+            return 0;
+
+        }
+
+        //add modifier
+        if (posModifier != string::npos) {
+
+            try {
+                modifier = stoi(expression.substr(posModifier + 1, expression.length() - 1)); //create substring for modifier and parse into integer
+
+            } catch (const exception& e) {
+                cerr << "Invalid modifier\n";
+                return 0;
+            }
+        }
+
+        int total = 0;
+        for (int i = 0; i < num; ++i) { // for number of times rolled, generates dice roll and adds it to total
+
+            total += rollOnce(sides);
+        }
+        int result = total + modifier;
+        results.push_back(result);
+        notifyObservers();
+        return result;
     }
 
-    int total = 0;
-    for (int i = 0; i < num; ++i) { // for number of times rolled, generates dice roll and adds it to total
-
-        total += rollOnce(sides);
+    vector<int>& getPastResults() {
+        return results;
     }
 
-    return total + modifier; // adds the total with the modifier for the final result
+};
 
-}
+class DiceObserver : Observer {
+public:
+    DiceObserver(Dice& dice) : dice(dice) {};
 
-//test
-int main() {
+    void update () { 
 
-    string str;
-    Dice dice;
+        vector<int>& pastResults = dice.getPastResults();
+        //cout << "Past dice roll results: " << endl;
+        for (int result : pastResults) {
+            cout << result << endl;
+        }
+        cout << endl;
+    }
 
-    str = "3d100[+9]"; //valid
-    //str = "6d5[+10]"; //invalid
+    // void printDiceLog () {
 
-    // cout << "Enter a dice expression in the form xdy[+z] : ";
-    // getline(cin, str); //makes sure that the entire string is consumed
-    int result = dice.rollDice(str);
-    cout << "Your result is " << result << endl;
+    //     vector<int>& pastResults = dice.getPastResults();
+    //     cout << "Past dice roll results: " << endl;
+    //     for (int result : pastResults) {
+    //         cout << result << endl;
+    //     }
+    //     cout << endl;
+    // }
 
-    return 0;
+private:
+    Dice& dice;
 
-}
+};
 
+// //test
+// int main() {
 
-// int DiceRoll::d4() {
-//     return (std::rand() % 4) + 1;
-// };
+//     string str;
+//     Dice dice;
+//     DiceObserver observer(dice);
 
-// int DiceRoll::d6() {
-//     return (std::rand() % 6) + 1;
-// };
+//     while (true) {
+//             string str;
+//             cout << "Enter a dice expression in the form xdy[+z] (enter 'q' to quit): ";
+//             getline(cin, str);
 
-// int DiceRoll::d8() {
-//     return (std::rand() % 8) + 1;
-// };
+//             if (str == "q") {
+//                 cout << "Exiting..." << endl;
+//                 observer.update(); //prints past result 
+//                 break; // Exit the loop
+//             }
 
-// int DiceRoll::d10() {
-//     return (std::rand() % 10) + 1;
-// };
+//             int result = dice.rollDice(str);
+//             cout << "Your result is " << result << endl;
+//     }
 
-// int DiceRoll::d12() {
-//     return (std::rand() % 12) + 1;
-// };
+//     return 0;
 
-// int DiceRoll::d20() {
-//     return (std::rand() % 20) + 1;
-// };
-
-// int DiceRoll::d100() {
-//     return (std::rand() % 100) + 1;
-// };
+// }
